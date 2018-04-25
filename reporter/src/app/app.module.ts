d2de7042e026aaf13fa89ac63898411094b01d5c
fjbatresv <fjbatresv@gmail.com>
2018-04-25 13:48:22 -0600

    Changes on the program, partners and loading icon

---
+++ reporter/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule, 
+    MatCheckboxModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
